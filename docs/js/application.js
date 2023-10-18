31f67098c8daf3c9862e1d926035bac3e89fcebf
Towsti <towsti1@gmail.com>
2023-10-18 21:03:56 +0200

    cleanup before modules refactor

--- docs/js/application.js
+++ docs/js/application.js
@@ -1,3 +1,6 @@
+// import { hello } from './github.js';
+
+
 async function rawGithubGetRequest(url) {
   const res = await fetch(url, {
       method: 'GET'
@@ -27,11 +30,16 @@ function populateServers(servers) {
 
 function populateEmojis(emojis) {
   const table = document.getElementById("table-emojis");
+  var count = 1;
   for (const emoji of emojis) {
+    count ++;
     let newRow = table.insertRow(-1);
     newRow.insertCell(-1).innerHTML = `<img  title="${emoji.emoji_name}" class="disc-emoji" src="https://cdn.discordapp.com/emojis/${emoji.emoji_id}.webp?v=1">&nbsp;&nbsp;${emoji.name}`;
     newRow.insertCell(-1).innerHTML = `<code><:${emoji.emoji_name}:${emoji.emoji_id}></code>`;
-    newRow.insertCell(-1).innerHTML = `${emoji.category}<button type="button" id="copyEmoji" class="btn btn-secondary btn-sm" data-bs-trigger="focus" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Copied!" onclick="copyEmojiToClipboard(this)" style="float:right;">Copy</button>`;
+    // newRow.insertCell(-1).innerHTML = `${emoji.category}<button type="button" id="copyEmoji" class="btn btn-secondary btn-sm" data-bs-trigger="focus" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Copied!" onclick="copyEmojiToClipboard(this)" style="float:right;">Copy</button>`;
+    newRow.insertCell(-1).innerHTML = `${emoji.category}<button type="button" id="copy-emoji-${count}" class="btn btn-secondary btn-sm" data-bs-trigger="focus" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Copied!" style="float:right;">Copy</button>`;
+    
+    document.querySelector(`#copy-emoji-${count}`).addEventListener('click', () => navigator.clipboard.writeText(`<:${emoji.emoji_name}:${emoji.emoji_id}>`), false)
   }
 
   const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
@@ -75,36 +83,61 @@ async function populateTables() {
   populateServers(servers);
 }
 
-function copyEmojiToClipboard(button) {
-  /* Copy emoji ID from table row of the pressed button. */
-  const row = button.parentNode.parentNode;
-  const emojiID = row.cells[2].innerText
-
-  navigator.clipboard.writeText(emojiID);
-}
-
 function selectSearchEmoji() {
   const searchBox = document.getElementById('search-emojis');
   searchBox.focus();
   searchBox.select();  
 }
 
-function updateEmojiTableResultCount(resultCount) {
-  $(".counter").text(`${resultCount} emojis`);
-}
+
 
 function selectTabOnPageLoad() {
   /* Select tab when loading page with pvme.io/pvme-settings#emojis. */
   const selectedTab = window.location.hash;
   if (selectedTab) {
     $(selectedTab).tab('show');
-
     if (selectedTab === '#emojis') selectSearchEmoji();
   }
 }
 
+function updateEmojiTableResultCount(resultCount) {
+  $("#count-emojis").text(`${resultCount} emojis`);
+}
+
+function searchEmojiTableRows() {
+  const searchTerm = $("#search-emojis").val();
+  const searchSplit = searchTerm.replace(/ /g, "'):containsi('");
+    
+  $.extend($.expr[":"], {
+    containsi: function (elem, i, match, array) {
+      return (
+        (elem.textContent || elem.innerText || "")
+          .toLowerCase()
+          .indexOf((match[3] || "").toLowerCase()) >= 0
+      );
+    }
+  });
+
+  $("#table-emojis tbody tr")
+    .not(":containsi('" + searchSplit + "')")
+    .each(function (e) {
+      $(this).attr("visible", "false");
+    });
+
+  $("#table-emojis tbody tr:containsi('" + searchSplit + "')").each(function () {
+    $(this).attr("visible", "true");
+  });
+  
+  const resultCount = $('#table-emojis tbody tr[visible="true"]').length;
+  updateEmojiTableResultCount(resultCount);
+
+  if (resultCount === 0) $(".no-result").show()
+  else $(".no-result").hide();
+}
+
 
 $(document).ready(function () {
+  
   populateTables();
 
   selectTabOnPageLoad();
@@ -121,39 +154,6 @@ $(document).ready(function () {
   });
 
   $("#search-emojis").keyup(function () {
-    var searchTerm = $("#search-emojis").val();
-    var searchSplit = searchTerm.replace(/ /g, "'):containsi('");
-    
-    console.log(searchSplit);
-    $.extend($.expr[":"], {
-      containsi: function (elem, i, match, array) {
-        return (
-          (elem.textContent || elem.innerText || "")
-            .toLowerCase()
-            .indexOf((match[3] || "").toLowerCase()) >= 0
-        );
-      }
-    });
-
-    $(".results tbody tr")
-      .not(":containsi('" + searchSplit + "')")
-      .each(function (e) {
-        $(this).attr("visible", "false");
-      });
-
-    $(".results tbody tr:containsi('" + searchSplit + "')").each(function () {
-      $(this).attr("visible", "true");
-    });
-    
-
-    const resultCount = $('.results tbody tr[visible="true"]').length;
-    updateEmojiTableResultCount(resultCount);
-    // $(".counter").text(jobCount + " emojis");
-
-    if (resultCount == "0") {
-      $(".no-result").show();
-    } else {
-      $(".no-result").hide();
-    }
+    searchEmojiTableRows();
   }); 
 });
