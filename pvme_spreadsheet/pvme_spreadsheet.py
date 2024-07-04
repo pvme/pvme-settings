55154c0dae3633c629987c69e862d9c94bf29791
Towsti <73656340+Towsti@users.noreply.github.com>
2024-07-04 09:33:13 +0000

    added cell alias parsing (named ranges)

--- pvme_spreadsheet/pvme_spreadsheet.py
+++ pvme_spreadsheet/pvme_spreadsheet.py
@@ -39,33 +39,52 @@ def get_json_lut(sheet: gspread.Spreadsheet):
     Drop        Drop per kill   Value   Gp/kill     ...     ...     ...
     Blood Rune  4.1356          985	    2,353       ...     ...     ...
     Coins       3,748.1958      1	    3,748       ...     ...     ...
-    Dragon      Arrowheads      1.3333  379387     ...     ...     ...
+    Dragon      Arrowheads      1.3333  379387      ...     ...     ...
 
     TO:
     {
-        'Slayer': {
-            'A': ['A', 'Nightmares', '...', 'Drop', 'Blood Rune', 'Coins', 'Dragon'],
-            'B': ['B', '...', '...', 'Drop per kill', '4.1356', '3,748.1958', 'Arrowheads'],
-            ...
+        "cells": {
+            "prices": {
+            "A": ["10", "20", "30"],
+            "B": ["30", "-1", "20,3"]
+            },
+            "Perks": {...}
         },
-        'Perks': {},
-        ...
+        "cell_aliases": {
+            "gptotal_archglacor_200ks": "20",
+            ...
+        }
     }
     """
-    lut = {}
+    # parse cells
+    cells = {}
+    worksheets_lut = {}     # id: title
     for worksheet in sheet.worksheets():
+        worksheets_lut[worksheet.id] = worksheet.title
+
         values = worksheet.get_all_values()
         compact_col_count = len(values[0])  # worksheet.col_count returns all columns (even those with no data)
         compact_row_count = len(values)     # worksheet.rol_count returns all rows
 
-        lut[worksheet.title] = {}
+        cells[worksheet.title] = {}
         for col_n in range(compact_col_count):
-            col_l = chr(col_n + 65)                 # 1 -> A
-            lut[worksheet.title][col_l] = [col_l]   # set index 0 to header 'A', 'B' so index for cells starts with 1
+            col_l = chr(col_n + 65)                     # 1 -> A
+            cells[worksheet.title][col_l] = [col_l]     # set index 0 to header 'A', 'B' so index for cells starts with 1
             for row_n in range(compact_row_count):
-                lut[worksheet.title][col_l].append(values[row_n][col_n])
+                cells[worksheet.title][col_l].append(values[row_n][col_n])
 
-    return lut
+    # parse cell aliases (named ranges)
+    cell_aliases = {}
+    for named_range in sheet.list_named_ranges():
+        try:
+            range_ = named_range['range']
+            cell_worksheet = cells[worksheets_lut[range_['sheetId']]]
+            cell_col = list(cell_worksheet.keys())[range_['startColumnIndex']]
+            cell_aliases[named_range['name']] = cell_worksheet[cell_col][range_['startRowIndex'] + 1]
+        except (KeyError, IndexError) as e:
+            print(f"cell alias not found: {e}")
+
+    return dict(cells=cells, cell_aliases=cell_aliases)
 
 
 def write_json_lut(json_lut):
